e47bf8a7f07e3cdb9478d2cb549e8fbaca6970e6
Alicia <alicia.zangger@gmail.com>
2018-11-23 11:35:22 +0100

    Created simple GUI inspired by previous work

---
+++ TDDImageProcessingApp/MainForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace TDDImageProcessingApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picPreview = new System.Windows.Forms.PictureBox();
+            this.btnOpenOriginal = new System.Windows.Forms.Button();
+            this.cmbFilters = new System.Windows.Forms.ComboBox();
+            this.cmbEdgeDetection = new System.Windows.Forms.ComboBox();
+            this.btnSaveNewImage = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picPreview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picPreview
+            // 
+            this.picPreview.BackColor = System.Drawing.Color.Silver;
+            this.picPreview.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.picPreview.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picPreview.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.picPreview.Location = new System.Drawing.Point(12, 12);
+            this.picPreview.Name = "picPreview";
+            this.picPreview.Size = new System.Drawing.Size(600, 600);
+            this.picPreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picPreview.TabIndex = 14;
+            this.picPreview.TabStop = false;
+            // 
+            // btnOpenOriginal
+            // 
+            this.btnOpenOriginal.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenOriginal.Location = new System.Drawing.Point(9, 629);
+            this.btnOpenOriginal.Name = "btnOpenOriginal";
+            this.btnOpenOriginal.Size = new System.Drawing.Size(150, 46);
+            this.btnOpenOriginal.TabIndex = 16;
+            this.btnOpenOriginal.Text = "Load Image";
+            this.btnOpenOriginal.UseVisualStyleBackColor = true;
+            // 
+            // cmbFilters
+            // 
+            this.cmbFilters.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbFilters.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbFilters.FormattingEnabled = true;
+            this.cmbFilters.Items.AddRange(new object[] {
+            "None",
+            "Rainbow",
+            "Black & white"});
+            this.cmbFilters.SelectedIndex = 0;
+            this.cmbFilters.Location = new System.Drawing.Point(172, 638);
+            this.cmbFilters.Name = "cmbFilters";
+            this.cmbFilters.Size = new System.Drawing.Size(132, 32);
+            this.cmbFilters.TabIndex = 21;
+            // 
+            // cmbEdgeDetection
+            // 
+            this.cmbEdgeDetection.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbEdgeDetection.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbEdgeDetection.FormattingEnabled = true;
+            this.cmbEdgeDetection.Items.AddRange(new object[] {
+            "None",
+            "Laplacian 3x3",
+            "Sobel 3x3"});
+            this.cmbEdgeDetection.SelectedIndex = 0;
+            this.cmbEdgeDetection.Location = new System.Drawing.Point(320, 638);
+            this.cmbEdgeDetection.Name = "cmbEdgeDetection";
+            this.cmbEdgeDetection.Size = new System.Drawing.Size(132, 32);
+            this.cmbEdgeDetection.TabIndex = 22;
+            // 
+            // btnSaveNewImage
+            // 
+            this.btnSaveNewImage.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveNewImage.Location = new System.Drawing.Point(464, 629);
+            this.btnSaveNewImage.Name = "btnSaveNewImage";
+            this.btnSaveNewImage.Size = new System.Drawing.Size(150, 46);
+            this.btnSaveNewImage.TabIndex = 23;
+            this.btnSaveNewImage.Text = "Save Image";
+            this.btnSaveNewImage.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(169, 615);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 20);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Image filter";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(316, 615);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 20);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Edge detection";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 685);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSaveNewImage);
+            this.Controls.Add(this.cmbEdgeDetection);
+            this.Controls.Add(this.cmbFilters);
+            this.Controls.Add(this.btnOpenOriginal);
+            this.Controls.Add(this.picPreview);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picPreview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picPreview;
+        private System.Windows.Forms.Button btnOpenOriginal;
+        private System.Windows.Forms.ComboBox cmbFilters;
+        private System.Windows.Forms.ComboBox cmbEdgeDetection;
+        private System.Windows.Forms.Button btnSaveNewImage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TDDImageProcessingApp/MainForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace TDDImageProcessingApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
