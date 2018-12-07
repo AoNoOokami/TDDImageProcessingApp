1e0d60e81c6998b20d877723f1928e6c74d97b4d
batterteam <batterteam@gmail.com>
2018-12-07 11:43:47 +0100

    Image input/output

---
+++ TDDImageProcessingApp/LoadImage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDDImageProcessingApp.Properties;
+
+namespace TDDImageProcessingApp
+{
+    class LoadImage : IloadImage
+    {
+        Bitmap IloadImage.LoadImage(Bitmap loadedImg)
+        {
+            return Resources.monkey; 
+        }
+    }
+}
